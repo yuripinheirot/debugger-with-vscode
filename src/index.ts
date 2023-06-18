ab8dc7ac18472f48ec7b581a30b343baecf8fea1
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-06-18 10:28:49 -0400

    chore: add debugger config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
